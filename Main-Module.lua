c84bdb51a0cc0bb0d0f66fe51e14268a9bcfca3d
dyumra (KUYHUB) <woraphan8871234@gmail.com>
2025-06-08 04:17:44 +0700

    Update Main-Module.lua

--- Main-Module.lua
+++ Main-Module.lua
@@ -2,7 +2,7 @@
 
 -- [[ ⚙️ Roblox Execution Module ]]
 -- [[ 🔮 Powered by Dyumra's Innovations ]]
--- [[ 📊 Version: 2.18.5 - Authenticated Interface Edition ]]
+-- [[ 📊 Version: 2.19.5 - Authenticated Interface Edition ]]
 -- [[ 🔗 Other Script : https://github.com/dyumra - Thank for Support ]]
 
 local Players = game:GetService("Players")
